c3cb44e36f743c4728213b103704320b53db4c75
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2026-09-09 17:10:39 +0200

    Remove unused opaque serializers for delta resolvers

--- serlib/ser_mod_subst.mli
+++ serlib/ser_mod_subst.mli
@@ -16,12 +16,5 @@
 (* Written by: Emilio J. Gallego Arias and others                       *)
 (************************************************************************)
 
-type delta_resolver = Mod_subst.delta_resolver
- [@@deriving sexp,yojson,hash,compare]
-
 type substitution = Mod_subst.substitution
  [@@deriving sexp,yojson,hash,compare]
-
-(* type 'a substituted = 'a Mod_subst.substituted
- * val sexp_of_substituted : ('a -> Sexp.t) -> 'a substituted -> Sexp.t
- * val substituted_of_sexp : (Sexp.t -> 'a) -> Sexp.t -> 'a substituted *)
